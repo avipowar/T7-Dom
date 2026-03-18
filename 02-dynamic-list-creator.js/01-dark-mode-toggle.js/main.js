330aeae8e8edb7164e8c8fea8fb6ea6470190c74
Avinash Powar <powaravinash06@gmail.com>
2026-03-18 16:06:03 +0530

    build dynamic list creator

--- 02-dynamic-list-creator.js/01-dark-mode-toggle.js/main.js
+++ 02-dynamic-list-creator.js/01-dark-mode-toggle.js/main.js
@@ -1,12 +1,63 @@
-const btn = document.getElementById("toggleButton")
+let input = document.getElementById("itemInput")
+let addBtn = document.getElementById("addBtn")
+let ulList = document.getElementById("list")
 
-
-btn.addEventListener("click", ()=> {
-    document.body.classList.toggle("light");
-    
-    if(document.body.classList.contains("light")){
-        btn.textContent = "Toggle Dark Mode";
-    } else {
-        btn.textContent = "Toggle Light Mode"
+addBtn.addEventListener("click", () => {
+    if(input.value === ""){
+        alert("add item")
+        return;
     }
+
+    // create li 
+    let li = document.createElement("li")
+    li.textContent = input.value;
+
+    // create Delete btn
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+    deleteBtn.classList.add("delete");
+
+    li.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener("click", ()=>{
+        li.remove()
+    })
+
+    // create edit btn 
+    li.addEventListener("dblclick", ()=>{
+        const oldText = li.firstChild.textContent.trim();
+        
+        let editInput = document.createElement("input");
+        editInput.type = "text";
+        editInput.value = oldText
+        
+
+        li.insertBefore(editInput, deleteBtn)
+        li.firstChild.remove();
+
+        editInput.focus();
+
+        const save = () => {
+            const editText = editInput.value;
+            
+            const textNode = document.createTextNode(editText);
+
+            li.replaceChild(textNode, editInput)
+        }
+
+
+        editInput.addEventListener("keydown", (e) => {
+           if(e.key === "Enter"){
+                save()
+           }
+        })
+
+        editInput.addEventListener("blur", (e) => {
+                save()
+        })
+
+    })
+    
+    ulList.appendChild(li);
+    input.value = "";
 })
